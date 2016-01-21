1a028d9370dcd6331201f9fd0f244567f0f05f38
koopr2 <koopr2@nku.edu>
2016-01-20 21:17:41 -0500

    Initial Commit

---
+++ lab1.rb
@@ -0,0 +1,9 @@
+# Answers to question 1
+puts 1024 * 8
+# Answers to question 2
+puts 1024 * 1024
+# Answers to question 3
+puts 8 * 1024 * 1024 * 30
+# Answers to question 4
+puts 912000000 / 8 / 1024 / 1024
+puts 912000000.0 / 8.0 / 1024.0 / 1024.0
